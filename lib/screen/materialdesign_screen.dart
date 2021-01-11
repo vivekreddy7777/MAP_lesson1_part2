8be907be75c0e80e72a3125e4dd4942fdb4c5087
vivekreddy7777 <vivekreddy7777@gmail.com>
2021-01-11 04:47:57 -0600

    20

--- lib/screen/materialdesign_screen.dart
+++ lib/screen/materialdesign_screen.dart
@@ -8,57 +8,63 @@ class MaterialDesignScreen extends StatelessWidget {
         appBar: AppBar(
           title: Text('Material Design Style'),
         ),
-        body: Column(
-          children: [
-            Text(
-              'One',
-              style: TextStyle(
-                color: Colors.yellow,
-                fontSize: 24,
-                fontFamily: 'Couirier New',
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Text(
+                'One',
+                style: TextStyle(
+                  color: Colors.yellow,
+                  fontSize: 24,
+                  fontFamily: 'Couirier New',
+                ),
               ),
-            ),
-            Text(
-              'headline1',
-              style: Theme.of(context).textTheme.headline1,
-            ),
-            Text(
-              'headline2',
-              style: Theme.of(context).textTheme.headline2,
-            ),
-            Text(
-              'headline3',
-              style: Theme.of(context).textTheme.headline3,
-            ),
-            Text(
-              'headline4',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            Text(
-              'headline5',
-              style: Theme.of(context).textTheme.headline5,
-            ),
-            Text(
-              'bodyText1',
-              style: Theme.of(context).textTheme.bodyText1,
-            ),
-            Text(
-              'bodyText2',
-              style: Theme.of(context).textTheme.bodyText2,
-            ),
-            Text(
-              'botton',
-              style: Theme.of(context).textTheme.button,
-            ),
-            Text(
-              'caption',
-              style: Theme.of(context).textTheme.caption,
-            ),
-            Text(
-              'overline',
-              style: Theme.of(context).textTheme.overline,
-            ),
-          ],
+              Text(
+                'headline1',
+                style: Theme.of(context).textTheme.headline1,
+              ),
+              Text(
+                'headline1-1',
+                style: Theme.of(context).textTheme.headline1,
+              ),
+              Text(
+                'headline2',
+                style: Theme.of(context).textTheme.headline2,
+              ),
+              Text(
+                'headline3',
+                style: Theme.of(context).textTheme.headline3,
+              ),
+              Text(
+                'headline4',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+              Text(
+                'headline5',
+                style: Theme.of(context).textTheme.headline5,
+              ),
+              Text(
+                'bodyText1',
+                style: Theme.of(context).textTheme.bodyText1,
+              ),
+              Text(
+                'bodyText2',
+                style: Theme.of(context).textTheme.bodyText2,
+              ),
+              Text(
+                'botton',
+                style: Theme.of(context).textTheme.button,
+              ),
+              Text(
+                'caption',
+                style: Theme.of(context).textTheme.caption,
+              ),
+              Text(
+                'overline',
+                style: Theme.of(context).textTheme.overline,
+              ),
+            ],
+          ),
         ));
   }
 }

1bad23b64f809cefa6cd7224449bf5c9e370d0de
vivekreddy7777 <vivekreddy7777@gmail.com>
2021-01-11 06:15:02 -0600

    23

--- lib/screen/image_screen.dart
+++ lib/screen/image_screen.dart
@@ -9,7 +9,25 @@ class ImageScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Image Demo Screen'),
       ),
-      body: Text('Image'),
+      body: ListView(
+        children: [
+          Container(
+              height: 200.0,
+              color: Colors.blueAccent,
+              child: Image.network(
+                  'https://m.media-amazon.com/images/I/81fA3NNiBRL._AC_UL480_QL65_.jpg')),
+          Icon(
+            Icons.ac_unit,
+            size: 100.0,
+            color: Colors.red,
+          ),
+          Container(
+              height: 200.0,
+              color: Colors.teal,
+              child: Image.network(
+                  'https://images-na.ssl-images-amazon.com/images/I/51HylLbOAJL._SX404_BO1,204,203,200_.jpg')),
+        ],
+      ),
     );
   }
 }

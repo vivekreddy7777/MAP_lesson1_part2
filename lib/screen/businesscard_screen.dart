b411627237156b940188c30a1caddd404895275f
vivekreddy7777 <vivekreddy7777@gmail.com>
2021-01-18 03:48:13 -0600

    23

--- lib/screen/businesscard_screen.dart
+++ lib/screen/businesscard_screen.dart
@@ -9,60 +9,84 @@ class BusinessCardScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('My Business Card'),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Center(
-            child: ClipOval(
-              child: Image.asset(
-                'images/profile.jpg',
-                height: 200.0,
-              ),
-            ),
-          ),
-          Text(
-            'Name',
-            style: Theme.of(context).textTheme.headline6,
-          ),
-          Text(
-            'Vivek Reddy',
-            style: Theme.of(context).textTheme.headline4,
-          ),
-          Text(
-            'Title',
-            style: Theme.of(context).textTheme.headline6,
-          ),
-          Text(
-            'Mr',
-            style: Theme.of(context).textTheme.headline4,
-          ),
-          Text(
-            'Department',
-            style: Theme.of(context).textTheme.headline6,
-          ),
-          Text(
-            'Computer Science',
-            style: Theme.of(context).textTheme.headline4,
-          ),
-          Row(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(30.0, 50.0, 30.0, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Icon(Icons.email),
+              Center(
+                child: ClipOval(
+                  child: Image.asset(
+                    'images/profile.jpg',
+                    height: 200.0,
+                  ),
+                ),
+              ),
+              Divider(
+                height: 40.0,
+                color: Colors.yellow,
+              ),
               Text(
-                'vmandadi@uco.edu',
-                style: Theme.of(context).textTheme.headline5,
+                'Name',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              Text(
+                'Vivek Reddy',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                'Title',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              Text(
+                'Mr',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                'Department',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              Text(
+                'Computer Science',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Row(
+                children: [
+                  Icon(Icons.email),
+                  SizedBox(
+                    width: 30.0,
+                  ),
+                  Text(
+                    'vmandadi@uco.edu',
+                    style: Theme.of(context).textTheme.headline5,
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Icon(Icons.phone),
+                  SizedBox(
+                    width: 30.0,
+                  ),
+                  Text(
+                    '+1(405) 123-4567',
+                    style: Theme.of(context).textTheme.headline5,
+                  ),
+                ],
               ),
             ],
           ),
-          Row(
-            children: [
-              Icon(Icons.phone),
-              Text(
-                '+1(405) 123-4567',
-                style: Theme.of(context).textTheme.headline5,
-              ),
-            ],
-          ),
-        ],
+        ),
       ),
     );
   }
